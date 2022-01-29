973c6c6fdf63a32e2d0232c89d63545af7ac73d7
Bhavani Ravi <bhavaniravi@users.noreply.github.com>
2022-01-29 19:35:23 +0530

    Update script.py

--- whatsapp/script.py
+++ whatsapp/script.py
@@ -1,6 +1,6 @@
 import pyautogui as pg
 import pywhatkit as kit
-kit.sendwhatmsg_instantly("+919940189591","Hello from python")
+kit.sendwhatmsg_instantly("phone_number_here","Hello from python")
 
 #pg.press("tab")
 pg.press("tab")
